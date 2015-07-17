560e34156fa3d3b8b7a7587d1fd464e1c1e2a3fb
Matt Bargar <mattbargar@cloudkid.com>
2015-07-17 10:28:44 -0400

    Added glob pattern matching

--- README.md
+++ README.md
@@ -1,16 +1,21 @@
 # image-tiler
-npm module for tiling a large image into smaller tiles. It optionally generates overlapping tiles. Also can export a metadata JSON file in a format that other projects can use.
+node module for tiling a large image into smaller tiles. It optionally generates overlapping tiles. Also can export a metadata JSON file in a format that other projects can use. It accepts [glob patterns](https://github.com/isaacs/node-glob) for the set of files that can be passed in.
 ## Setup
-This depends on [ImageMagick](http://www.imagemagick.org/script/index.php) being properly installed.
+
 ```bash
-$ brew install imagemagick
-```
+npm install```
 
 ## Example Usage
 Take the `crystal_Zone3_Midground.png` file and create tiles at the default size, overlapping columns by 1 px (default) and rows by 0 px, and generate JSON.
 ```bash
-~/Sites/Porting/measurement-skillpack-frontend/assets/zone/crystal_Zone3_Midground.png --out ~/Sites/Porting/measurement-skillpack-frontend/deploy/assets/images/zones/ --overlapRow 0 --exportJSON --jsonSrcPrefix assets/images/zones/
+./index.js --in ~/Sites/Project/assets/zone/crystal_Zone3_Midground.png --out ~/Sites/Project/deploy/assets/images/zones/ --overlapRow 0 --exportJSON --jsonSrcPrefix assets/images/zones/
 ```
+
+Take all PNG files in the `zone` folder and make 256x256px tiles with no overlap. Note the quotes - we are using a glob.
+```bash
+./index.js --in "~/Sites/Project/zone/*.png" --out ~/Sites/Project/deploy/assets/images/zones/ --width 256 --height 256 --overlap 0
+```
+
 ## Options
 | option     | format                                       | example                                | Default if omitted        |
 |:-----------|:---------------------------------------------|:---------------------------------------|:--------------------------|
@@ -25,6 +30,4 @@ Take the `crystal_Zone3_Midground.png` file and create tiles at the default size
 | exportJSON | (existence)                                  |                                        | false                     |
 
 ## TODO
-* remove dependency on ImageMagick (investigate [png-crop](https://www.npmjs.com/package/png-crop))
-* take a glob as a command-line option
 
--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 var fs = require('fs'),
 	path = require('path'),
 	zpad = require('zpad'),
+	untildify = require('untildify'),
 	glob = require("glob"),
 	PNG = require('pngjs').PNG,
 	PNGCrop = require('png-crop'),
@@ -21,94 +22,104 @@ if ((myArgs.h) || (myArgs.help))
 // ./index.js --in ~/Desktop/crystal_Zone1_Background.png --width 512 --height 768 --overlap 1 --out ~/Desktop/ --outBase crystal_Zone1_BG
 // ./index.js --in ~/Sites/Porting/measurement-skillpack-frontend/assets/zone/crystal_Zone3_Midground.png --out ~/Sites/Porting/measurement-skillpack-frontend/deploy/assets/images/zones/ --overlapRow 0 --exportJSON --jsonSrcPrefix assets/images/zones/
 //TODO error checking for existence of params
-//TODO take a glob
-var inFile = myArgs.in;
-
+var inFiles = untildify(myArgs.in);
 var tileWidth = myArgs.width || 1024;
 var tileHeight = myArgs.height || 1024;
 var overlap = myArgs.overlap || 1;
 var outFolder = myArgs.out;
-var extension = path.extname(inFile);
-var outBasename = myArgs.outBase || path.basename(inFile, extension);
 var shouldPrintJSON = myArgs.hasOwnProperty("exportJSON");
 var jsonSrcPrefix = myArgs.jsonSrcPrefix || "assets/images/zones/";
-
 var overlapCol = myArgs.hasOwnProperty("overlapCol") ? myArgs.overlapCol : overlap; //we can overlap x and y separately
 var overlapRow = myArgs.hasOwnProperty("overlapRow") ? myArgs.overlapRow : overlap;
+// console.log("GLOB:", inFiles);
 
-fs.createReadStream(inFile)
-	.pipe(new PNG(
+glob(inFiles, function(err, files)
+{
+	// console.log("FILES:", files);
+	files.forEach(function(file)
 	{
-		filterType: -1
-	}))
-	.on('metadata', function()
-	{
-		// console.log(this);
-		var libName = path.basename(inFile, extension);
-		var outObj = {
-			name: libName,
-			libName: libName,
-			position:
-			{
-				x: 0,
-				y: 0
-			},
-			depth: -1,
-			sliceSize:
-			{
-				width: tileWidth,
-				height: tileHeight,
-				overlap: overlap,
-				overlapCol: overlapCol,
-				overlapRow: overlapRow
-			},
-			images: []
-		};
-		var numRows = Math.ceil(this.height / (tileHeight - overlapRow));
-		var numCols = Math.ceil(this.width / (tileWidth - overlapCol));
-		// console.log("rows:", numRows, "cols:", numCols, file.height, tileHeight);
-		var iter = 0;
-		var imageArr = [];
+		var extension = path.extname(file);
+		var outBasename = myArgs.outBase || path.basename(file, extension);
 
-		var tileCallback = function(err) {
-			if (err) throw err;
-		};
-		for (var row = 0; row < numRows; row++)
-		{
-			var rowImages = [];
-			for (var col = 0; col < numCols; col++)
+		fs.createReadStream(file)
+			.pipe(new PNG(
 			{
-				var dst;
+				filterType: -1
+			}))
+			.on('metadata', function()
+			{
+				// console.log(this);
+				var libName = path.basename(file, extension);
+				var outObj = {
+					name: libName,
+					libName: libName,
+					position:
+					{
+						x: 0,
+						y: 0
+					},
+					depth: -1,
+					sliceSize:
+					{
+						width: tileWidth,
+						height: tileHeight,
+						overlap: overlap,
+						overlapCol: overlapCol,
+						overlapRow: overlapRow
+					},
+					images: []
+				};
+				var numRows = Math.ceil(this.height / (tileHeight - overlapRow));
+				var numCols = Math.ceil(this.width / (tileWidth - overlapCol));
+				// console.log("rows:", numRows, "cols:", numCols, file.height, tileHeight);
+				var iter = 0;
+				var imageArr = [];
 
-
-				var destinationFilename = outBasename + "_" + zpad(iter) + extension;
-				dst = path.join(outFolder, destinationFilename);
-				var x = (col * tileWidth) - (col * overlapCol);
-				var y = (row * tileHeight) - (row * overlapRow);
-				rowImages.push(
+				var tileCallback = function(err)
 				{
-					id: path.basename(destinationFilename, extension),
-					src: jsonSrcPrefix + destinationFilename
-				});
+					if (err) throw err;
+				};
+				for (var row = 0; row < numRows; row++)
+				{
+					var rowImages = [];
+					for (var col = 0; col < numCols; col++)
+					{
+						var dst;
+						var destinationFilename = outBasename + "_" + zpad(iter) + extension;
+						dst = path.join(outFolder, destinationFilename);
+						var x = (col * tileWidth) - (col * overlapCol);
+						var y = (row * tileHeight) - (row * overlapRow);
+						rowImages.push(
+						{
+							id: path.basename(destinationFilename, extension),
+							src: jsonSrcPrefix + destinationFilename
+						});
 
-				var config = {width: tileWidth, height: tileHeight, left: x, top: y};
-				PNGCrop.crop(inFile, dst, config, tileCallback);
-				
-				iter++;
-			}
-			imageArr.push(rowImages);
-		}
-		outObj.images = imageArr;
-		if (shouldPrintJSON)
-		{
-			var jsonStr = JSON.stringify(outObj, null, 4);
-			// console.log(jsonStr);
-			var jsonFilePath = path.join(outFolder, libName + ".json");
-			fs.writeFile(jsonFilePath, jsonStr, function(err, written, string)
-			{
-				if (err) throw err;
-				console.log("done with", libName);
-			});
-		}
+						var config = {
+							width: tileWidth,
+							height: tileHeight,
+							left: x,
+							top: y
+						};
+						PNGCrop.crop(file, dst, config, tileCallback);
 
-	});
+						iter++;
+					}
+					imageArr.push(rowImages);
+				}
+				outObj.images = imageArr;
+				if (shouldPrintJSON)
+				{
+					var jsonStr = JSON.stringify(outObj, null, 4);
+					// console.log(jsonStr);
+					var jsonFilePath = path.join(outFolder, libName + ".json");
+					fs.writeFile(jsonFilePath, jsonStr, function(err, written, string)
+					{
+						if (err) throw err;
+						console.log("done with", libName);
+					});
+				}
+
+			}); //end of metadata per file
+	}); //end of files.forEach
+}); //end of glob
